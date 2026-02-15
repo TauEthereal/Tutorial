7098e61e58b2d773aeb08ee3ba7e0369c4bfefe5
TauEthereal <alyksitov@gmail.com>
2026-02-16 00:44:57 +0800

    внесён черновик конвертера покемонов

--- Hello world.py
+++ Hello world.py
@@ -1,4 +1,33 @@
-print("Hello world)
-print("I change it! ") 
-print("I change it from PC")
-print("You must see that when i pushed it from my phone") 
+Height, Weight, Frnd, Catch, Health, Atk,Def, SpAtk, SpDef, Spd = map(int, input("Введите в строгой последовательности значения Height Weight Frnd Catch Health Atk Def SpAtk SpDef целым числом через пробел").split())
+#Этап 1. Цикл для Роста
+x1=Height
+Рост = None
+МРУ = None
+МРБ = None
+
+if 0.1 <= x1 <= 0.49:
+    Рост = "Крошечный"
+    МРУ = 8
+    МРБ = -2
+elif 0.5 <= x1 <=0.99:
+    Рост = "Маленький"
+    МРУ = 5
+    МРБ = -2
+elif 1.0 <= x1 <= 1.49:
+    Рост = "Ниже среднего"
+    МРУ = 3
+    МРБ = -1
+elif 1.5 <= x1 <= 1.99:
+    Рост = "Средний"
+    МРУ = 0
+    МРБ = 0
+elif 2.0 <= x1 <= 2.99:
+    Рост = "Высокий"
+    МРУ = -6
+    МРБ = +1
+else:
+    a = 0
+    b = 0
+
+# Теперь a и b определены и готовы к дальнейшим расчётам
+print(f"a = {a}, b = {b}")
