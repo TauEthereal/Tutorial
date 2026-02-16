d8ca7d59ee3e8db59f9f665a972bfff75aa4ecb9
TauEthereal <alyksitov@gmail.com>
2026-02-16 20:52:14 +0800

    написан генератор дружбы, отправляю на правку

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import my_functions.height_coefficients
 import my_functions.weight_coefficients
 Height, Weight, Frnd, Catch, Health, Atk,Def, SpAtk, SpDef, Spd = map(int, input("Введите в строгой последовательности значения Height (в метрах) Weight Frnd Catch Health Atk Def SpAtk SpDef целым числом через пробел").split())
 #Этап 1. Цикл для Роста
-Рост,МРУ,МРБ = height_coefficients.heights(Height)
+Рост,МРУ,МРБ = my_functions.height_coefficients.heights(Height)
+print(Рост)
 #Этап 2. Цикл для Массы
-Масса, ММУ, ММБ,ММЗ = weight_coefficients.weights(Weight)
+Масса, ММУ, ММБ,ММЗ = my_functions.weight_coefficients.weights(Weight)
 print(Масса)
+#Этап 3. Цикл для Дружелюбия
+Дружелюбие = my_functions.frnd_coefficients.frnds(Frnd)
