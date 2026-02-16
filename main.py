6be09425981bc8850a9f18d0ad1f192ff6884687
TauEthereal <alyksitov@gmail.com>
2026-02-16 18:53:19 +0800

    Создан модуль для расчета веса покемонов, создана отдельная папка с функциями и привязана к главному файлу.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import height_coefficients
+import my_functions.height_coefficients
+import my_functions.weight_coefficients
 Height, Weight, Frnd, Catch, Health, Atk,Def, SpAtk, SpDef, Spd = map(int, input("Введите в строгой последовательности значения Height (в метрах) Weight Frnd Catch Health Atk Def SpAtk SpDef целым числом через пробел").split())
 #Этап 1. Цикл для Роста
-x1=Height
-Рост,МРУ,МРБ = height_coefficients.heights(x1)
-print(Рост,МРУ,МРБ)
+Рост,МРУ,МРБ = height_coefficients.heights(Height)
+#Этап 2. Цикл для Массы
+Масса, ММУ, ММБ,ММЗ = weight_coefficients.weights(Weight)
+print(Масса)
