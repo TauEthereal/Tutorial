f29792e1776b8fe2aceb2ecdb1156a98540a0f50
TauEthereal <alyksitov@gmail.com>
2026-02-18 02:05:16 +0800

    добавлено много чего, таблицы, этапы

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import my_functions.height_coefficients
 import my_functions.weight_coefficients
 import my_functions.frnd_coefficients
+import my_functions.growth_coefficients
 Height, Weight, Frnd, Catch, Health, Atk,Def, SpAtk, SpDef, Spd = map(int, input("Введите в строгой последовательности значения Height (в метрах) Weight Frnd Catch Health Atk Def SpAtk SpDef целым числом через пробел").split())
+Growth = input("Введите значение атрибута Growth Rate на английском, как-то: Medium Fast")
 #Этап 1. Цикл для Роста
 Рост,МРУ,МРБ = my_functions.height_coefficients.heights(Height)
 print(Рост)
@@ -11,3 +13,8 @@ print(Масса)
 #Этап 3. Цикл для Дружелюбия
 Дружелюбие = my_functions.frnd_coefficients.frnds(Frnd)
 print(Дружелюбие)
+#Этап 4. Цикл для теста Опыта
+XP = my_functions.growth_coefficients.growths(Growth)
+print(XP)
+#Этап 5. Цикл для поимки
+CR = 
