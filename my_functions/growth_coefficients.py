24b9f9720759c2923b741810c49606378f749e3e
TauEthereal <alyksitov@gmail.com>
2026-02-18 20:51:24 +0800

    Работа над ошибками
    Исправил строку кода с else в новом модуле

--- my_functions/growth_coefficients.py
+++ my_functions/growth_coefficients.py
@@ -1,14 +1,15 @@
 def growths(Growth):
-    if Growth = "Fluctuating":
+    if Growth == "Fluctuating":
      XP = ("Тест Опыта 1к20")
-    elif Growth = "Slow":
+    elif Growth == "Slow":
      XP = ("Тест Опыта 2к10")
-    elif Growth = "Medium Slow":
+    elif Growth == "Medium Slow":
      XP = ("Тест Опыта 3к8")
-    elif Growth = "Medium Fast":
+    elif Growth == "Medium Fast":
      XP = ("Тест Опыта 5к6")
-    elif Growth = "Fast":
+    elif Growth == "Fast":
      XP = ("Тест Опыта 6к5")
-    else Growth = "Erratic":
-     XP = ("Тест Опыта 3к12")
+    else:
+      Growth == "Erratic"
+      XP = ("Тест Опыта 3к12")
     return XP
