100e77508723c25a8c646565c48d7e584317e305
TauEthereal <alyksitov@gmail.com>
2026-02-18 08:10:08 +0800

    изменил

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import my_functions
 Height, Weight, Frnd, Catch, Health, Atk,Def, SpAtk, SpDef, Spd = map(int, input("Введите в строгой последовательности значения Height (в метрах) Weight Frnd Catch Health Atk Def SpAtk SpDef целым числом через пробел").split())
 Growth = input("Введите значение атрибута Growth Rate на английском, как-то: Medium Fast")
 #Этап 1. Цикл для Роста
-Рост,МРУ,МРБ = my_functions.height_coefficients.heights(Height)
+Рост,МРУ,МРБ = my_functions.heights(Height)
 print(Рост)
 #Этап 2. Цикл для Массы
 Масса, ММУ, ММБ,ММЗ = my_functions.weight_coefficients.weights(Weight)
